a1d325ccbcf0b9084a07ad68ea9212220c398109
Maksim <uzhas76064@gmail.com>
2024-12-06 13:46:19 +0300

    добавил типы пропсов

---
+++ src/types/interfaces.ts
@@ -0,0 +1,9 @@
+import {Place} from './type';
+
+export interface MainProps {
+  places: Place[];
+}
+
+export interface AppProps {
+  places: Place[];
+}
---
+++ src/types/type.ts
@@ -0,0 +1,7 @@
+export type Place = {
+  price: number;
+  inBookmarks: boolean;
+  rating: number;
+  photo: string;
+  href: string;
+}
