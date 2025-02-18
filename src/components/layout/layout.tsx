800a085258d49c1e8f94e1b90aa20616b2f980a4
Maksim <uzhas76064@gmail.com>
2025-02-18 11:38:49 +0300

    изменил путь

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -22,12 +22,10 @@ const Layout = (): JSX.Element => {
                 <li className="header__nav-item user">
                   <Link
                     className="header__nav-link header__nav-link--profile"
-                    to="#"
+                    to="/favorites"
                   >
                     <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                                        Oliver.conner@gmail.com
-                  </span>
+                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
                     <span className="header__favorite-count">3</span>
                   </Link>
                 </li>
