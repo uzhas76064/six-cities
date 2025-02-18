fdad0d874dbb80be013627b0644ff2c1c5c19575
Maksim <uzhas76064@gmail.com>
2025-02-18 10:37:34 +0300

    добавил роутинг

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,26 @@
 import { Main } from '../../pages/main/main';
+import {Route, Routes} from "react-router-dom";
+import {Login} from "../../pages/login/login";
+import Layout from "../layout/layout";
+import {Favorites} from "../../pages/favorites/favorites";
+import NotFound from "../../pages/not-found/not-found";
 
 type AppProps = {
   offersCount: number
 }
 
 function App({ offersCount }: AppProps): JSX.Element {
-  return <Main offersCount={offersCount} />;
+  return (
+  <Routes>
+    <Route path="/" element={<Layout/>}>
+      <Route index element={<Main offersCount={offersCount} />}/>
+      <Route path="login" element={<Login/>}/>
+      <Route path="favorites" element={<Favorites/>}/>
+      <Route path="offer/:id"/>
+    </Route>
+    <Route path="*" element={<NotFound/>}/>
+  </Routes>
+)
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+import {BrowserRouter} from "react-router-dom";
 
 const CARDS_COUNT = 5;
 
@@ -10,6 +11,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App offersCount={CARDS_COUNT}/>
+    <BrowserRouter>
+      <App offersCount={CARDS_COUNT}/>
+    </BrowserRouter>
   </React.StrictMode>,
 );
