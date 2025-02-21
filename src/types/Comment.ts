6238df388514e2baa83535227b83d564b8a20abd
Maksim <uzhas76064@gmail.com>
2025-02-21 10:49:30 +0300

    добавил типы

---
+++ src/types/Comment.ts
@@ -0,0 +1,9 @@
+import {User} from "./User";
+
+export type Comment = {
+  comment: string
+  date: string
+  id: number
+  rating: number
+  user: User
+}
---
+++ src/types/Favorite.ts
@@ -0,0 +1,22 @@
+import {City} from "./City";
+import {Host} from "./Host";
+import {Location} from "./Location";
+
+export type Favorite = {
+  bedrooms: number
+  city: City
+  description: string
+  goods: string[]
+  host: Host
+  id: number
+  images: string[]
+  isFavorite: boolean
+  isPremium: boolean
+  location: Location
+  maxAdults: number
+  previewImage: string
+  price: number
+  rating: number
+  title: string
+  type: string
+}
