914fc5b80fc6de137141940c8dcc1f7fa8858255
mgkhvatov <125257679+mgkhvatov@users.noreply.github.com>
2025-03-12 13:32:18 +0300

    добавил названия городов и их координаты

---
+++ src/const.ts
@@ -0,0 +1,36 @@
+import {CityName, Location} from "./types/Location";
+
+export const cities = ['Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'] as const;
+
+export const CityLocation: { [key in CityName]: Location } = {
+  'Paris': {
+    latitude: 48.85661,
+    longitude: 2.351499,
+    zoom: 13
+  },
+  'Cologne': {
+    latitude: 50.938361,
+    longitude: 6.959974,
+    zoom: 13
+  },
+  'Brussels': {
+    latitude: 50.846557,
+    longitude: 4.351697,
+    zoom: 13
+  },
+  'Amsterdam': {
+    latitude: 52.37454,
+    longitude: 4.897976,
+    zoom: 13
+  },
+  'Hamburg': {
+    latitude: 53.550341,
+    longitude: 10.000654,
+    zoom: 13
+  },
+  'Dusseldorf': {
+    latitude: 51.225402,
+    longitude: 6.776314,
+    zoom: 13
+  },
+};
--- src/types/Location.ts
+++ src/types/Location.ts
@@ -1,5 +1,14 @@
+import {cities} from "../const";
+
+export type CityName = typeof cities[number];
+
 export type Location = {
   latitude: number
   longitude: number
   zoom: number
+}
+
+export type City = {
+  name: CityName,
+  location: Location
 }
