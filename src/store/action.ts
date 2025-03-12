2e5621120d7ac5a8e6f451d119b22a91558c7124
mgkhvatov <125257679+mgkhvatov@users.noreply.github.com>
2025-03-12 13:38:01 +0300

    переименовал

--- src/store/action.ts
+++ src/store/action.ts
@@ -2,5 +2,5 @@ import {createAction} from "@reduxjs/toolkit";
 import {CityName} from "../types/Location";
 import { Offer } from "../types/Offer";
 
-export const chooseCity = createAction<CityName>("offers/chooseCity");
-export const fillOffers = createAction<Offer[]>("offers/fillOffers");
+export const setCity = createAction<CityName>("offers/chooseCity");
+export const setOffers = createAction<Offer[]>("offers/fillOffers");
