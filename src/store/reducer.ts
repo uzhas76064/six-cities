1d8bf11a1f1e22c8cc17f6929dc34c4b089ca4af
mgkhvatov <125257679+mgkhvatov@users.noreply.github.com>
2025-03-14 13:44:41 +0300

    добавил состояние

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -2,11 +2,13 @@ import {createReducer} from "@reduxjs/toolkit";
 import {setCity, setOffers} from "./action";
 import {Offer} from "../types/Offer";
 import {City} from "../types/Location";
-import {cities, CityLocation} from "../const";
+import {cities, CityLocation, sortingParams} from "../const";
+import {SortingParams} from "../types/SortingParams";
 
 export type State = {
   city: City,
-  offers: Offer[]
+  offers: Offer[],
+  sortingParams: SortingParams
 }
 
 const initialState: State = {
@@ -14,7 +16,16 @@ const initialState: State = {
     name: cities[0],
     location: CityLocation[cities[0]]
   },
-  offers: []
+  offers: [],
+  sortingParams: {
+    default: 0,
+    params: {
+      0: "Popular",
+      1: "Price: low to high",
+      2: "Price: high to low",
+      3: "Top rated first"
+    }
+  }
 }
 
 export const reducer = createReducer(initialState, (builder) => {
