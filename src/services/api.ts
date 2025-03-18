3daafcbd7ed9bdd3f50f485c395962f6a90cd79e
Maksim <uzhas76064@gmail.com>
2025-03-18 14:44:15 +0300

    добавил перехватчика для перехвата авторизационного токена

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
 import axios, {AxiosInstance} from "axios";
+import {getToken} from "./token";
 
 const BASE_URL = "https://10.react.htmlacademy.pro/six-cities";
 const REQUEST_TIMEOUT = 5000;
@@ -7,6 +8,17 @@ export const createAPI = (): AxiosInstance => {
   const api = axios.create({
     baseURL: BASE_URL,
     timeout: REQUEST_TIMEOUT,
+  })
+
+  api.interceptors.request.use(config => {
+    const token = getToken();
+
+    if(token) {
+      // @ts-ignore
+      config.headers['x-token'] = token;
+    }
+
+    return config;
   })
 
   return api;
