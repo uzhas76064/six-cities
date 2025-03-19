7dd84860a0410f2df7b8ada44e07be93fd2dc3a7
Maksim <uzhas76064@gmail.com>
2025-03-19 12:30:27 +0300

    добавил метод для отправки комментария

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,7 @@
 import axios, {AxiosInstance} from "axios";
 import {getToken} from "./token";
 import {Routes} from "../const";
+import {Comment} from "../types/Comment";
 
 const BASE_URL = "https://10.react.htmlacademy.pro/six-cities";
 const REQUEST_TIMEOUT = 5000;
@@ -43,4 +44,10 @@ export const fetchNearbyPlaces = async (id: number) => {
   const api = createAPI();
 
   return await api.get(`${Routes.HOTELS}/${id}/nearby`);
+}
+
+export const postComment = async (id: number, comment: Comment) => {
+  const api = createAPI();
+
+  return await api.post(`${Routes.COMMENTS}/${id}`, comment);
 }
